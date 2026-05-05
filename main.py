f0bba1cb55fd8ba6613bf171b0a7b5ca8ccf54e2
Mimoja <git@mimoja.de>
2026-05-05 22:15:42 +0200

    + 28 minutes: implement undo and priorities
    Signed-off-by: Mimoja <git@mimoja.de>

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ TODOS_FILE_NAME = os.getenv("TODOS_FILE", "todos.json")
 TODOS_FILE = os.path.join(TODOS_PATH, TODOS_FILE_NAME)
 
 todos = []
+journal = []
 
 
 def getUTCTimestamp() -> float:
@@ -22,44 +23,81 @@ class Todo:
     description: str
     context: str | None
     completed_on: float | None
+    priority: str | None
 
     def __init__(
-        self, id: int, description: str, context: str | None, completed_on: float | None
+        self,
+        id: int,
+        description: str,
+        context: str | None = None,
+        completed_on: float | None = None,
+        priority: str | None = "middle",
     ):
         self.id = id
         self.description = description
         self.context = context if context is not None and context != "" else None
         self.completed_on = completed_on
+        self.priority = priority or "middle"
+
+    def toJSON(self):
+        return self.__dict__
+
+
+class JournalEntry:
+    action: str
+    todo_id: int
+    pre_todo: Todo | None
+
+    def __init__(self, action: str, todo_id: int, pre_todo: Todo | None | dict = None):
+        self.action = action
+        self.todo_id = todo_id
+        if isinstance(pre_todo, dict):
+            pre_todo = Todo(**pre_todo)
+        self.pre_todo = pre_todo
 
     def toJSON(self):
         return {
-            "id": self.id,
-            "description": self.description,
-            "context": self.context,
-            "completed_on": self.completed_on,
+            "action": self.action,
+            "todo_id": self.todo_id,
+            "pre_todo": self.pre_todo.toJSON() if self.pre_todo else None,
         }
 
 
 def writeTodos():
     tmpfile = os.path.join(TODOS_PATH, "_tmp.json")
 
+    journal_to_safe = journal[-10:]
+    data = {
+        "journal": [j.toJSON() for j in journal_to_safe],
+        "todos": [todo.toJSON() for todo in todos],
+    }
+
     with open(tmpfile, mode="w+") as f:
-        f.write(json.dumps([todo.__dict__ for todo in todos], indent=2))
+        f.write(json.dumps(data, indent=2))
 
     os.replace(tmpfile, TODOS_FILE)
 
 
 def readTodos():
     global todos
+    global journal
 
     if not os.path.exists(TODOS_FILE):
         todos = []
+        journal = []
         writeTodos()
         return
 
     try:
         with open(TODOS_FILE) as f:
-            todos = [Todo(**todo) for todo in json.load(f)]
+            json_data = json.load(f)
+            if isinstance(json_data, list):
+                journal = []
+                todos = [Todo(**todo) for todo in json_data]
+                writeTodos()
+            else:
+                journal = [JournalEntry(**j) for j in json_data.get("journal", [])]
+                todos = [Todo(**todo) for todo in json_data.get("todos", [])]
     except Exception as e:
         print(
             f"I am sad, everything is sad. Repair {TODOS_FILE} and try again! Error:",
@@ -86,10 +124,15 @@ def list_todos():
                 pass
 
         print(
+            f"[{todo.id}]",
             completed,
             todo.description,
-            tag=str(todo.id),
-            tag_color="green" if todo.completed_on is not None else "yellow",
+            tag=todo.priority or "default",
+            tag_color=(
+                "green"
+                if todo.completed_on is not None
+                else "red" if todo.priority == "high" else "yellow"
+            ),
             color="white",
             end="",
         )
@@ -106,8 +149,9 @@ def add_todo(desc: str, context: str | None):
 
     for description in descriptions:
         next_id = max([todo.id for todo in todos] or [0]) + 1
-        todos.append(Todo(next_id, description, context, None))
+        todos.append(Todo(next_id, description, context, None, "middle"))
 
+        journal.append(JournalEntry("add", next_id, None))
     writeTodos()
 
 
@@ -118,6 +162,7 @@ def mark_done(id: int):
         print("No TODO found with this ID", color="red")
         return
 
+    journal.append(JournalEntry("done", todos[todo_ind[0]].id, todos[todo_ind[0]]))
     todos[todo_ind[0]].completed_on = getUTCTimestamp()
 
     writeTodos()
@@ -128,7 +173,59 @@ def del_todo(id: int):
     if not todo_ind:
         print("No TODO found with this ID", color="red")
         return
+    journal.append(JournalEntry("delete", todos[todo_ind[0]].id, todos[todo_ind[0]]))
     del todos[todo_ind[0]]
+
+    writeTodos()
+
+
+def prio_todo(id: int, priority: str):
+    global todos
+    todo_ind = [indx for indx, todo in enumerate(todos) if todo.id == id]
+    if not todo_ind:
+        print("No TODO found with this ID", color="red")
+        return
+
+    journal.append(JournalEntry("prio", todos[todo_ind[0]].id, todos[todo_ind[0]]))
+    todos[todo_ind[0]].priority = priority
+
+    writeTodos()
+
+
+def undo_action():
+    global journal
+
+    if not journal:
+        print("No actions to undo", color="red")
+        exit()
+
+    last_action = journal[-1]
+    print(
+        f"Undoing {last_action.action} for todo no {last_action.todo_id}",
+        color="magenta",
+    )
+
+    todo_ind = [
+        indx for indx, todo in enumerate(todos) if todo.id == last_action.todo_id
+    ]
+
+    if last_action.action == "add":
+        if not todo_ind:
+            print("Invalid journal state, cannot roll back", color="red")
+            return
+        del todos[todo_ind[0]]
+    elif last_action.action == "delete":
+        if not last_action.pre_todo:
+            print("Invalid journal state, cannot roll back", color="red")
+            return
+        todos.append(last_action.pre_todo)
+    else:
+        if not todo_ind or not last_action.pre_todo:
+            print("Invalid journal state, cannot roll back", color="red")
+            return
+
+        todos[todo_ind[0]] = last_action.pre_todo
+    journal.pop()
     writeTodos()
 
 
@@ -139,6 +236,7 @@ def main():
 
     # List parser without args
     subparser.add_parser("list")
+    subparser.add_parser("undo")
 
     add_parser = subparser.add_parser("add")
     add_parser.add_argument(
@@ -162,6 +260,16 @@ def main():
         "ID", help="ID of the TODO item (use the list argument)", type=int
     )
 
+    prio_parser = subparser.add_parser("prio")
+    prio_parser.add_argument(
+        "ID", help="ID of the TODO item (use the list argument)", type=int
+    )
+    prio_parser.add_argument(
+        "Priority",
+        help="ID of the TODO item (use the list argument)",
+        type=str,
+        choices=["low", "middle", "high"],
+    )
     args = parser.parse_args()
 
     readTodos()
@@ -169,10 +277,14 @@ def main():
     match args.command:
         case "list":
             list_todos()
+        case "undo":
+            undo_action()
         case "add":
             add_todo(args.description, args.context)
         case "delete":
             del_todo(args.ID)
+        case "prio":
+            prio_todo(args.ID, args.Priority)
         case "done":
             mark_done(args.ID)
         case _:
